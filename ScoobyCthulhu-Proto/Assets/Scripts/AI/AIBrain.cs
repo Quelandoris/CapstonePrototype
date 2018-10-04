c1e6c79c035f3d22df064ed1eb8fe6f098e6c0e1
Unknown <aws140230@utdallas.edu>
2018-10-04 14:11:59 -0500

    AIPassiveWalk now works, and GitIgnore updated to ignore Library (Cache)

--- ScoobyCthulhu-Proto/Assets/Scripts/AI/AIBrain.cs
+++ ScoobyCthulhu-Proto/Assets/Scripts/AI/AIBrain.cs
@@ -1,54 +1,101 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIBrain : MonoBehaviour {
 
     //General Variables
-    AIMove myAIMove = new AIMove();
-	
+    public Transform NavTarget;
+    NavMeshAgent agent;
+    public enum modes { Passive, Alert, Chase};
+    public modes mode;
+
     //AI Passive Walk
-	public Dictionary<Transform, int> NavTargets = new Dictionary<Transform,int>();
+    Transform[] NavTargets;
 
     // Use this for initialization
     void OnEnable () {
-        myAIMove = GetComponent<AIMove>();
-		AIEventManager.NavReached += NavTargetReached;
-        //Convert Targets into NavTargetPriorities, and give them the default priority = 50
+        mode = modes.Passive;
+        agent = GetComponent<NavMeshAgent>();
+        //Fill NavTargets
+        int loopnum = 0;
+        NavTargetScript[] temp = FindObjectsOfType<NavTargetScript>();
+        NavTargets = new Transform[temp.Length];
+        //foreach (NavTargetScript s in FindObjectsOfType<NavTargetScript>())
+        foreach (NavTargetScript s in temp)
+        {
+            NavTargets[loopnum] = s.GetComponent<Transform>();
+            loopnum++;
+        }
                 
 	}
 	
 	void OnDisable(){
-		AIEventManager.NavReached -= NavTargetReached;
+        //reset NavTargets in case some get disabled between this object getting disabled and reenabled
+        Array.Clear(NavTargets, 0, NavTargets.Length);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	void NavTargetReached(){
-		if(myAIMove.NavTarget==tran){
-			changeTarget();
-		}
-	}
-	
+
+    //The Devil
+    //Calls what mode AI is in. Resource heavy, so the individual modes are meant to be fairly light
+    void Update()
+    {
+        switch (mode)
+        {
+            case modes.Passive:
+                AIPassiveWalk();
+                break;
+            case modes.Alert:
+
+                break;
+            case modes.Chase:
+
+                break;
+            default:
+
+                break;
+        }
+    }
+
     //AIPassiveWalk is used when the AI has no other task it is persuing; it will patrol between different points depending on how long it has been since it last went to that point, with some RNG
     //It does this until an Event call changes its state.
     void AIPassiveWalk()
     {
-        if(myAIMove.NavTarget==null){
+        //If there is no navTarget (Usually at start of game or if the navtarget gets disabled) find a new navtarget
+        if (NavTarget==null){
 			changeTarget();
 		}
 		
     }
-	
-	void changeTarget(){
-		Transform highestPriority;
-		foreach(Transform x in NavTargets.GetKeys){
-			if(NavTargets[x].GetValue>highestPriority) highestPriority=x;
+    //Called by NavTargetScript on collision with Monster
+    public void NavTargetReached(Transform tr)
+    {
+        if (NavTarget == tr)
+        {
+            //Reset current nav's priority
+            NavTarget.GetComponent<NavTargetScript>().priority = 0;
+            //choose new navtarget
+            changeTarget();
+        }
+    }
+    //Selects new TargetNavPoint from Array based on what has highest Priority
+    void changeTarget()
+    {
+        Transform highestPriority = NavTargets[0];
+		foreach(Transform x in NavTargets){
+            //Prevent nullref
+            if (x.GetComponent<NavTargetScript>())
+            {
+                //Compare priority, if true reassign highestPriority
+                if (x.GetComponent<NavTargetScript>().priority > highestPriority.GetComponent<NavTargetScript>().priority)
+                {
+                    highestPriority = x;
+                }
+            }
 		}
-		
-		myAIMove.NavTarget = highestPriority;
-	}
+		//After highestPriority is found, assign it to myAIMove
+		NavTarget = highestPriority;
+        agent.destination = NavTarget.position;
+    }
 }

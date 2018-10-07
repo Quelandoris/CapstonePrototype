146f4e23fe60c19b6a616fabf5e478dcd5815546
Unknown <aws140230@utdallas.edu>
2018-10-07 16:42:05 -0500

    Fireplace in

---
+++ ScoobyCthulhu-Proto/Assets/Scripts/FireplaceScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireplaceScript : MonoBehaviour {
+
+    public GameObject fireplaceTrap; //The trap version of the fireplace
+    public GameObject Fireplace; //The unlit fireplace
+
+    // Use this for initialization
+    void OnTriggerEnter (Collider coll) {
+        if (coll.tag == "Oil")
+        {
+            Debug.Log("Coll");
+            coll.gameObject.SetActive(false);
+            fireplaceTrap.SetActive(true);
+            Fireplace.SetActive(false);
+        }
+	}
+	
+}
---
+++ ScoobyCthulhu-Proto/Assets/Scripts/Trap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+
+    public GameObject winPanel;
+    public GameObject Fireplace; //The unlit fireplace
+    public GameObject fireplaceTrap; //The trap version of the fireplace
+    public int LitTime; //Time the fireplace stays lit after the oil hits it (Seconds)
+
+    private void OnEnable()
+    {
+        Invoke("Disable", LitTime);
+    }
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "Monster")
+        {
+            //Kill the monster
+            coll.gameObject.SetActive(false);
+            //Turn on the win panel
+            winPanel.SetActive(true);
+
+        }
+    }
+    void Disable()
+    {
+        Fireplace.SetActive(true);
+        fireplaceTrap.SetActive(false);
+    }
+}

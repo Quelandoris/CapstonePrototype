1418b2088aacd9b3c70fbd3d3e529c84a40f9cdf
Unknown <cenesbitt3@gmail.com>
2018-10-07 13:55:29 -0500

    Dog
    added dog bark
    added dog fetch
    added noise volumes

--- ScoobyCthulhu-Proto/Assets/Scripts/AI/DogMovement.cs
+++ ScoobyCthulhu-Proto/Assets/Scripts/AI/DogMovement.cs
@@ -7,19 +7,31 @@ public class DogMovement : MonoBehaviour {
 
     public Transform NavTarget;
     NavMeshAgent agent;
-    public enum modes { Passive, Fetch, Stay, Bark };
+    public enum modes { Passive, Fetch, Stay};
     public modes mode;
     private GameObject player;
     public float MoveSpeed;
+    public bool GoFetch=false;
+    public bool Fetching = false;
+    Vector3 point;
+    int FloorMask;//to see where the mouse is
+    public GameObject NoisePrefab;//to spawn multiple noisePrefabs for effect might change for a particle system later
+    public GameObject NoisePrefab1;
+    public GameObject NoisePrefab2;
 
     void OnEnable () {
         mode = modes.Passive;
+        FloorMask = LayerMask.GetMask("Floor");
         player = GameObject.FindGameObjectWithTag("Player");
         agent = GetComponent<NavMeshAgent>();
     }
 	
 	// switches what mode Dog AI is in
 	void Update () {
+        if (GoFetch)//to communicate with player when they click left mouse
+        {
+            mode = modes.Fetch;
+        }
         switch (mode)
         {
             case modes.Passive:
@@ -31,13 +43,16 @@ public class DogMovement : MonoBehaviour {
             case modes.Stay:
                 DogStay();
                 break;
-            case modes.Bark:
-                DogBark();
-                break;
+  
             default:
-                Debug.Log("Mode Switch in AIBrain Update broke somehow.");
-                break;
+            break;
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mode = modes.Passive;
+            DogBark();
+        }
+        
     }
     //DogPassive is used when the dog is in its default state following the player
     void DogPassive()
@@ -58,6 +73,42 @@ public class DogMovement : MonoBehaviour {
     }
     void DogFetch()
     {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit TargetHit;
+        
+        if (Physics.Raycast(camRay, out TargetHit, 100f, FloorMask))
+        {
+            if (Fetching)
+            {
+                point = TargetHit.point;
+                Fetching = false;
+            }
+        }
+
+        try
+        {
+         //make the dog go to where the mouse is
+            if (Vector3.Distance(gameObject.transform.position, point) <= 2f)//if it reaches destination
+            {
+                if (Input.GetAxisRaw("Fire2") == 1)
+                {
+                    GoFetch = false;
+                    mode = modes.Passive;
+                }
+                
+            }
+            else
+            {
+                //Debug.Log("error");
+                agent.SetDestination(point);
+            }
+        }
+        catch
+        {
+
+        }
+
+        
 
     }
     void DogStay()
@@ -66,6 +117,8 @@ public class DogMovement : MonoBehaviour {
     }
     void DogBark()
     {
-
+        Instantiate(NoisePrefab,gameObject.transform);
+        Instantiate(NoisePrefab1, gameObject.transform);
+        Instantiate(NoisePrefab2, gameObject.transform);
     }
 }

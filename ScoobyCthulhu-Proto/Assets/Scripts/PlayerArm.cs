158d2abbbf9421197455e0091ced8e24be4bfec0
Unknown <cenesbitt3@gmail.com>
2018-10-07 21:29:10 -0500

    inventory done

---
+++ ScoobyCthulhu-Proto/Assets/Scripts/PlayerArm.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArm : MonoBehaviour {
+
+    public GameObject oilProjPrefab;
+	void Start () {
+		
+	}
+	
+	
+	public void Shoot()
+    {
+        
+        Instantiate(oilProjPrefab,transform.position,oilProjPrefab.transform.rotation);
+    }
+}

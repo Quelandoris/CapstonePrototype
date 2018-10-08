636bd978576709e79fcae52c3e3ec413ed4e8511
Unknown <cenesbitt3@gmail.com>
2018-10-07 20:48:16 -0500

    added inventory and oil and oil bottle

---
+++ ScoobyCthulhu-Proto/Assets/Scripts/Oil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oil : MonoBehaviour {
+
+    Transform transform;
+    public float GrowthRate = 2f;
+    public float time = .5f;
+    void Awake () {
+        transform = GetComponent<Transform>();
+    }
+	
+	
+	void Update () {
+        time -= Time.deltaTime;
+        if (time >= 0)
+        {
+           
+                transform.localScale += new Vector3(GrowthRate, 0f, GrowthRate) * Time.deltaTime;
+            
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}

0e88eca8cb2325a15fc75d2272306f500a869a80
Unknown <aws140230@utdallas.edu>
2018-10-07 22:10:04 -0500

    ResiCamera is working
    Simple to implement and setup as part of the level design process

---
+++ ScoobyCthulhu-Proto/Assets/Scripts/Level/CameraVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraVolume : MonoBehaviour {
+
+    public Camera myCam;
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "Player")
+        {
+            myCam.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider coll)
+    {
+        if (coll.tag == "Player")
+        {
+            myCam.gameObject.SetActive(false);
+        }
+    }
+}
